3245d0676da007959bd9a91c4aea1824f6360d65
guzhengyang <1141374791@qq.com>
2020-04-26 17:02:03 +0800

    1st

---
+++ 18308045_hw3_谷正阳/test1.m
@@ -0,0 +1,27 @@
+sz = 1000;
+km = 1000;
+gap = 100;
+A = rand(sz);
+B = rand(sz, 1);
+P0 = zeros(sz, 1);
+k = gap:gap:km;
+t1 = zeros(size(k));
+t2 = zeros(size(k));
+for i = 1:size(k, 2)
+    tic
+    gauss_seidel(A, B, P0, k(i));
+    t1(i) = toc;
+end
+for i = 1:size(k, 2)
+    tic
+    gauss_seidel_parallel(A, B, P0, k(i));
+    t2(i) = toc;
+end
+kk = gap:km;
+tt1 = spline(k, t1, kk);
+tt2 = spline(k, t2, kk);
+hold on
+plot(kk, tt1)
+plot(kk, tt2)
+legend('gauss_seidel','gauss_seidel_parallel');
+claer
